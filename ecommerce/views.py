70b5ac33c76103bdbd286e472c6834e103e5585b
leiwu123 <jobspecific@hotmail.com>
2018-08-19 12:00:51 +0800

    added contact/view and a simple form

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,21 +3,29 @@ from django.shortcuts import render
 
 def home_page(request):
   context = {
-    "title":"Hello World!"
+    "title":"Hello World!",
+    "content": "welcome to the homepage!"
   }
   return render(request, "home_page.html", context)
 
 def about_page(request):
   context = {
-    "title":"About page!"
+    "title":"About page!",
+    "content": "welcome to the homepage!"
   }
   return render(request, "home_page.html", context)
 
 def contact_page(request):
   context = {
-    "title":"Contact page!"
+    "title":"Contact page!",
+    "content": "welcome to the homepage!"
   }
-  return render(request, "home_page.html", context)
+  if request.method == "POST":
+    print(request.POST)
+    print(request.POST.get('fullname'))
+    print(request.POST.get('email'))
+    print(request.POST.get('content'))
+  return render(request, "contact/view.html", context)
 
 def home_page_old(request):
   html_ = """
