c3fded8b63d2dc590773e5ce8cbf7f42092fa4a2
leiwu123 <jobspecific@hotmail.com>
2018-08-19 17:27:19 +0800

    added authentication login for premium content

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,48 +1,81 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm
+
 
 def home_page(request):
-  context = {
-    "title":"Hello World!",
-    "content": "welcome to the homepage!"
-  }
-  return render(request, "home_page.html", context)
+    context = {
+        "title": "Hello World!",
+        "content": "welcome to the homepage!"
+    }
+
+    if request.user.is_authenticated():
+      context["premium_content"] = "YEAHHHHH great content!"
+    return render(request, "home_page.html", context)
+
 
 def about_page(request):
-  context = {
-    "title":"About page!",
-    "content": "welcome to the homepage!"
-  }
-  return render(request, "home_page.html", context)
+    context = {
+        "title": "About page!",
+        "content": "welcome to the homepage!"
+    }
+    return render(request, "home_page.html", context)
+
 
 def contact_page(request):
-  contact_form = ContactForm(request.POST or None)
-  context = {
-    "title":"Contact page!",
-    "content": "welcome to the homepage!",
-    "form": contact_form
-  }
-  if contact_form.is_valid():
-    print(contact_form.cleaned_data)
-  # if request.method == "POST":
-  #   print(request.POST)
-  #   print(request.POST.get('fullname'))
-  #   print(request.POST.get('email'))
-  #   print(request.POST.get('content'))
-  return render(request, "contact/view.html", context)
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title": "Contact page!",
+        "content": "welcome to the homepage!",
+        "form": contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #   print(request.POST)
+    #   print(request.POST.get('fullname'))
+    #   print(request.POST.get('email'))
+    #   print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
+
 
 def login_page(request):
-  
-  return render(request, "auth/login.html", {})
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    # print("User logged in")
+    print(request.user.is_authenticated())
+    if form.is_valid():
+        # print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        # print(request.user.is_authenticated())
+
+        if user is not None:
+            login(request, user)
+            # context['form'] = LoginForm()
+            return redirect("/login")
+        else:
+            print("error")
+
+    return render(request, "auth/login.html", context)
+
 
 def register_page(request):
-  
-  return render(request, "auth/register.html", {})
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "auth/register.html")
+
 
 def home_page_old(request):
-  html_ = """
+    html_ = """
   <!doctype html>
 <html lang="en">
   <head>
@@ -69,4 +102,4 @@ def home_page_old(request):
 </html>
   """
 
-  return HttpResponse(html_)
+    return HttpResponse(html_)
