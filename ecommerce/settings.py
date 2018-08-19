4698a2c567b110cb6720b1d694b7412def9cdc28
leiwu123 <jobspecific@hotmail.com>
2018-08-20 06:40:24 +0800

    added static and media roots

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -120,5 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static-my-proj"),
+    os.path.join(BASE_DIR, "static_my_proj"),
 ]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
