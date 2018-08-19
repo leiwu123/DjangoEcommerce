60e2e7fcf5aba51596b62e202531ebd8fe15ccf6
leiwu123 <jobspecific@hotmail.com>
2018-08-19 15:43:21 +0800

    added class based form validations for contact form

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def home_page(request):
   context = {
     "title":"Hello World!",
@@ -16,16 +18,28 @@ def about_page(request):
   return render(request, "home_page.html", context)
 
 def contact_page(request):
+  contact_form = ContactForm(request.POST or None)
   context = {
     "title":"Contact page!",
-    "content": "welcome to the homepage!"
+    "content": "welcome to the homepage!",
+    "form": contact_form
   }
-  if request.method == "POST":
-    print(request.POST)
-    print(request.POST.get('fullname'))
-    print(request.POST.get('email'))
-    print(request.POST.get('content'))
+  if contact_form.is_valid():
+    print(contact_form.cleaned_data)
+  # if request.method == "POST":
+  #   print(request.POST)
+  #   print(request.POST.get('fullname'))
+  #   print(request.POST.get('email'))
+  #   print(request.POST.get('content'))
   return render(request, "contact/view.html", context)
+
+def login_page(request):
+  
+  return render(request, "auth/login.html", {})
+
+def register_page(request):
+  
+  return render(request, "auth/register.html", {})
 
 def home_page_old(request):
   html_ = """
