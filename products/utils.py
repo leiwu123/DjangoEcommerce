7716dc22543ff81c5b97ec974bbf758b764f069a
leiwu123 <jobspecific@hotmail.com>
2018-08-23 18:15:29 +0800

    added search query

--- products/utils.py
+++ products/utils.py
@@ -1,17 +1,14 @@
 import random
 import string
-
 from django.utils.text import slugify
-
 
 
 def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+# print(random_string_generator())
 
-print(random_string_generator())
-
-print(random_string_generator(size=50))
+# print(random_string_generator(size=50))
 
 
 def unique_slug_generator(instance, new_slug=None):
