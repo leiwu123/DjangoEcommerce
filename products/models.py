fa3a0de198a482395332e677bf23420d11dff564
leiwu123 <jobspecific@hotmail.com>
2018-08-24 11:05:01 +0800

    added search by tags

--- products/models.py
+++ products/models.py
@@ -33,7 +33,10 @@ class ProductQuerySet(models.query.QuerySet):
   def search(self, query):
     lookups = (Q(title__icontains=query) | 
               Q(description__icontains=query) |
-              Q(price__icontains=query))
+              Q(price__icontains=query) |
+              Q(tag__title__icontains=query)
+              # Q(tag__slug__icontains=query)
+              )
     # Q(tag__name__icontains=query)
     return self.filter(lookups).distinct()
 
